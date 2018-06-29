51420f6954768c8c2584847c34949e8acc8833eb
fredowinz23 <fredowinz23@gmail.com>
2018-06-29 18:58:39 +0800

    Added layout

--- app/src/main/java/com/ph/confession/fragments/HotFrag.kt
+++ app/src/main/java/com/ph/confession/fragments/HotFrag.kt
@@ -2,10 +2,8 @@ package com.ph.confession.fragments
 
 import com.ph.confession.R
 
-
 class HotFrag : ABaseFrag() {
     override val TAG = HotFrag::class.java.simpleName
-    override val LAYOUT_ID = R.layout.fragment_hot
-
+    override val LAYOUT_ID = R.layout.hot_rv
 
 }
